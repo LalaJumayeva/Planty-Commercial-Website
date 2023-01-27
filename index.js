9ad7bbbeffae5b3ea4d2701a4340c77ae469fc9e
Lala Jumayeva <100639782+LalaJumayeva@users.noreply.github.com>
2023-01-27 17:58:10 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("navbar");
+const closebutton = document.getElementById("close");
+
+if(hamburger) {
+    hamburger.addEventListener('click', () => {
+        nav.classList.add("active");
+    })
+}
+
+if(closebutton) {
+    closebutton.addEventListener("click", function(){
+        nav.classList.remove("active");
+    })
+}
