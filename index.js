c4c3b2e302cdadd9f2ce2cbe69a7247d3a9527e1
Lala Jumayeva <100639782+LalaJumayeva@users.noreply.github.com>
2023-01-30 02:16:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("navbar");
 const closebutton = document.getElementById("close");
 
-
 if(hamburger) {
     hamburger.addEventListener('click', () => {
         nav.classList.add("active");
@@ -13,4 +12,15 @@ if(closebutton) {
     closebutton.addEventListener("click", function(){
         nav.classList.remove("active");
     })
+}
+
+// single product page
+var main_img = document.getElementById("mainimg");
+var otherimages = document.getElementsByClassName("small-img");
+
+
+for (let i = 0; i < otherimages.length; i++) {
+    otherimages[i].onclick = function(){
+    main_img.src = otherimages[i].src
+    }
 }
