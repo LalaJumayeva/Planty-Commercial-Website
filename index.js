81f35d11e58d2df93c6873d6fa1e4f2491dc9079
Lala Jumayeva <100639782+LalaJumayeva@users.noreply.github.com>
2023-02-03 19:44:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,41 @@ if(closebutton) {
         nav.classList.remove("active");
     })
 }
+ 
+
+// popups
+document.querySelector(".show-login").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+  });
+  
+  document.querySelector(".popup .close-btn").addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active");
+  })
+  
+  document.querySelector(".show-signup").addEventListener("click", function () {
+    document.querySelector(".popup-2").classList.add("active");
+  });
+  
+  document.querySelector(".popup-2 .close-btn").addEventListener("click", function () {
+    document.querySelector(".popup-2").classList.remove("active");
+  })
+
+  document.querySelector(".show-login2").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+  });
+  
+  document.querySelector(".popup .close-btn").addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active");
+  })
+  
+  document.querySelector(".show-signup2").addEventListener("click", function () {
+    document.querySelector(".popup-2").classList.add("active");
+  });
+  
+  document.querySelector(".popup-2 .close-btn").addEventListener("click", function () {
+    document.querySelector(".popup-2").classList.remove("active");
+  })
+  
 
 // single product page
 var main_img = document.getElementById("mainimg");
@@ -24,3 +59,5 @@ for (let i = 0; i < otherimages.length; i++) {
     main_img.src = otherimages[i].src
     }
 }
+
+
